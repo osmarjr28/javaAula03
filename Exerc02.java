aaa126171645efb92f8736ea0c54bfd61840261b
Osmar Junior <osmar.cardoso@mercadolivre.com>
2022-11-01 10:58:55 -0300

    My exercise 02 classrom 03

---
+++ Exerc02.java
@@ -0,0 +1,33 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class Exerc02 {
+
+	public static Scanner read = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int age;
+		
+		for(int i = 0; i <= 10; i ++) {
+			System.out.println("\nDigite sua idade (numero inteiro):");
+			age = read.nextInt();
+			
+		if(age < 21) {
+			System.out.println("\nTotal de pessoas menores de 21 anos:");
+			
+		
+		if(age > 51) {
+			System.out.println("\nTotal de pessoas maiores de 50 anos:");
+			
+		}else {
+			
+				}
+			}
+		}
+	}
+}
+
+
+
